205857fdcc52af828fcf2ed36e3c725f64b16029
1aoxomoxoa1 <atcohen60@gmail.com>
2023-10-22 19:23:32 +0300

    build added

---
+++ storybook-static/assets/Form.stories-2028ab0a.js
@@ -0,0 +1,21 @@
+import{M as b}from"./Form-9c0342db.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";import"./Button-d99210dc.js";import"./LinksSection-eff84995.js";const f={title:"Example/MyForm",component:b,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var m,c,l;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(l=(c=a.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var p,u,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(i=(u=e.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,g,y;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(y=(g=o.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};const E=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,o as Small,E as __namedExportsOrder,f as default};
+//# sourceMappingURL=Form.stories-2028ab0a.js.map
---
+++ storybook-static/assets/LinksSection.stories-310379b7.js
@@ -0,0 +1,21 @@
+import{L as b}from"./LinksSection-eff84995.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";const x={title:"Example/LinksSection",component:b,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},r={args:{primary:!0,label:"Button"}},e={args:{label:"Button"}},a={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var c,l,m;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(m=(l=e.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var i,p,u;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var d,g,S;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(S=(g=o.parameters)==null?void 0:g.docs)==null?void 0:S.source}}};const _=["Primary","Secondary","Large","Small"];export{a as Large,r as Primary,e as Secondary,o as Small,_ as __namedExportsOrder,x as default};
+//# sourceMappingURL=LinksSection.stories-310379b7.js.map
